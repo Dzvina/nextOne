7b8d9bfa0c0752ea8c4dfb9ad5ed78582ede2cdc
matya <matyashdo@gmail.com>
2019-03-20 12:04:05 +0100

    12345

--- src/main/java/com/mdo/utils/HibernateUtil.java
+++ src/main/java/com/mdo/utils/HibernateUtil.java
@@ -1,9 +1,9 @@
 package com.mdo.utils;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
 
@@ -13,8 +13,8 @@ public class HibernateUtil {
     public static final SessionFactory createSessionFactory() {
         Configuration configuration = new Configuration();
         configuration.configure();
-        serviceRegistry = new ServiceRegistryBuilder().applySettings(
-                configuration.getProperties()).buildServiceRegistry();
+        serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                configuration.getProperties()).build();
         sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         return sessionFactory;
     }
