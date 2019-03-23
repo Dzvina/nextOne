946992aa422464dda98e3d377c4eed6516fc340a
matya <matyashdo@gmail.com>
2019-03-23 22:14:19 +0100

    test commit

---
+++ src/main/java/com/mdo/model/Login.java
@@ -0,0 +1,58 @@
+package com.mdo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "login")
+public class Login {
+
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @Id
+    @Column(name = "password")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Login)) return false;
+        Login login = (Login) o;
+        return username.equals(login.username) &&
+                password.equals(login.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Login{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
